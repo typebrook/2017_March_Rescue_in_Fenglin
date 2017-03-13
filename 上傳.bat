048a44a1924ccb3e9e8519e413b25aebe68b770e
typebrook <typebrook@gmail.commit>
2017-03-13 18:05:45 +0800

    ''

--- 上傳.bat
+++ 上傳.bat
@@ -1,4 +1,6 @@
 cd %~dp0
+git config --global user.email typebrook@gmail.commit
+git config --global user.name typebrook
 git add -A
 git commit -a --allow-empty-message -m ''
 git push origin master
---
+++ 上傳_.bat
@@ -0,0 +1,5 @@
+cd %~dp0
+git add -A
+git commit -a --allow-empty-message -m ''
+git push origin master
+pause
